ae7b6d7962b0bc19a64e5b523ed88e9f86498be1
aniketghavte <104782783+aniketghavte@users.noreply.github.com>
2023-04-30 19:52:19 +0530

    home

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,8 +1,49 @@
 import React from 'react'
+import './Home.css'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='app__home'>
+        <div className='gt_timetable'>
+            <Link to={`/GT`}>
+                <div>
+                    <p>CS Class B</p>
+                </div>
+                <div style={{textAlign: "center"}}>
+                    <p>Time Generated</p>
+                    <p>14 SEPT</p>
+                </div>
+            </Link>
+        </div>
+        <div className='gt_timetable'>
+            <div>
+                <p>CS Class B</p>
+            </div>
+            <div style={{textAlign: "center"}}>
+                <p>Time Generated</p>
+                <p>14 SEPT</p>
+            </div>
+        </div>
+        <div className='gt_timetable'>
+            <div>
+                <p>CS Class B</p>
+            </div>
+            <div style={{textAlign: "center"}}>
+                <p>Time Generated</p>
+                <p>14 SEPT</p>
+            </div>
+        </div>
+        <div className='gt_timetable'>
+            <div>
+                <p>CS Class B</p>
+            </div>
+            <div style={{textAlign: "center"}}>
+                <p>Time Generated</p>
+                <p>14 SEPT</p>
+            </div>
+        </div>
+    </div>
   )
 }
 
